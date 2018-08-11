5fe94bd7af06a6c361b61119686cbf94612d7bae
vm <vmartinez72.vm@gmail.com>
2018-08-10 17:18:41 -0700

    add a new style of form.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
-if(yes) {
-  $("#start").show();
-}
+$(document).ready(function(){
+  $("#form-group").submit(function() {
 
-event.preventDefault();
+  })
+})
+
+
+
+event.preventDefault()
+// if(yes) {
+//   $("#start").show();
+// }
+//
+// $("#survey").click(function(event) {
+// event.preventDefault();
+//  var answers = ["question1", "person2", "animal", "exclamation", "verb", "noun"]
+//
+//  answers.forEach(function(answer) {
+//      var userInput = $("input." + blank).val();
+//      $("." + blank).text(userInput).val();
+//    });
