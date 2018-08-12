b764c0100cf358d55d53bcd416253b933383e542
vm <vmartinez72.vm@gmail.com>
2018-08-11 17:51:46 -0700

    update js script.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,27 @@
-// $(document).ready(function(){
-//   $("form-control").submit(function(event)
+$(document).ready(function() {
+  $(".backEnd").click(function() {
+    $("#backEndInfo").toggle();
+    $("#frontEndInfo").toggle();
+  });
 
-var answr1 = $("#challenge").val();
-var answr2 = $("tech").val();
-var answr3 = $("commuication").val();
-var answr4 = $("visual").val();
-var answr5 = $("break").val();
-
-if (answr1 === 1 && answr2 === 2 && answer 3 && answer === 5) {
-  $('#backEnd').show();
-
-} else {
-  $('#frontEnd').show();
-}
-
+  $(".frontkEnd").click(function() {
+    $("#showFrontInfo").show();
+    $("#hideFrontInfo").hide();
+  });
+});
 
 event.preventDefault()
+// var answr1 = $("#challenge").val();
+// var answr2 = $("tech").val();
+// var answr3 = $("commuication").val();
+// var answr4 = $("visual").val();
+// var answr5 = $("break").val();
+//
+// if (answr1 === 1 && answr2 === 2 && answer 3 && answer === 5) {
+//   $('#backEnd').show();
+// } else {
+//   $('#frontEnd').show();
+// }
+//
+//
+//  event.preventDefault()
