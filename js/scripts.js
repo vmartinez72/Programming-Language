02b70010b87bf16e8b4496deee8557bd085b7efa
Student <example@example.com>
2018-08-12 17:21:59 -0700

    add form for survey result and add button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,19 @@
-$(document).ready(function() {
-   $(".clickable2").click(function() {
-    $("#show").show();
-    $("#hide").hide();
-  });
-    $(".clickable1").click(function() {
-   $("#start").toggle();
-   $("#end").toggle();
-  });
+$(document).ready(function() {                          $(".clickable2").click(function() {
+  $("#show").toggle();
+  $("#hide").toggle();
+});
+$(".clickable1").click(function() {
+  $("#start").toggle();
+  $("#end").toggle();
+ });
 });
 
-
-//   $("backEnd").click(function(event) {
-//     alert(start);
-//     $("#backEndInfo").toggle();
-//     $("#frontEndInfo").toggle();
-//   });
+//  $("#answer").submit(funtion(event) {
+//     var answer = parseInt($(input#number).val());
 //
-
-//
-// event.preventDefault();
-// var answr1 = $("#challenge").val();
-// var answr2 = $("tech").val();
-// var answr3 = $("commuication").val();
-// var answr4 = $("visual").val();
-// var answr5 = $("break").val();
-//
-// if (answr1 === 1 && answr2 === 2 && answer 3 && answer === 5) {
-//   $('#backEnd').show();
-// } else {
-//   $('#frontEnd').show();
-// }
-//
-//
-//  event.preventDefault()
+//    if (answer >=3) {
+//     $('#result').show();
+//  }  else {
+//     $('notgoodresult').show();
+//  }
+// });
