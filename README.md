76b94e00cb769c57b6716d7e72cbccb745130201
Student <example@example.com>
2018-08-14 20:51:59 -0700

    show practice work on js

--- README.md
+++ README.md
@@ -1,28 +1,28 @@
-### Programming-Language
-by Victora Martinez Date: August 10, 2018
+## Programming-Language
+by Victoria Martinez Date: August 10, 2018
 
-###Description
+## Description
 
 This is a short survey to learn more about the different languages epicodus offers for those students interested in web development and application.
 
-##Setup/Installation Requirements
+## Setup/Installation Requirements
 
-1.Computer, desktop or laptop
-2.Internet
-3.HTTP server
+
+- Computer, desktop or laptop
+- Internet
+- HTTP server
 
 
 ##Technologies Used
 
-**Mac Computer
-**JavaScript
-**Jquery
-**Css
-**GitHub Software
-**Atom Text Editor
-**Gogle Fonts
-**Color Hex Color Code
+- Mac Computer
+- JavaScript
+- jQuery
+- Css
+- GitHub Software
+- Atom Text Editor
+- Google Fonts
+_ Color Hex Color Code
 
-#Legal
-Copyright (c)2018 Victoria Martinez, Epicodus 
-
+## Legal
+Copyright (c)2018 Victoria Martinez, Epicodus
--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,28 @@
-$(document).ready(function() {                          $(".clickable2").click(function() {
-  $("#show").toggle();
-  $("#hide").toggle();
-});
-$(".clickable1").click(function() {
-  $("#start").toggle();
-  $("#end").toggle();
- });
-});
+$(document).ready(function() {
+  $(".clickable2").click(function() {
+  // event.preventDefault();
+    $("#show").toggle();
+    $("#hide").toggle();
+  });
+  $(".clickable1").click(function() {
+    $("#start").toggle();
+    $("#end").toggle();
+   });
+  });
 
-//  $("#answer").submit(funtion(event) {
-//     var answer = parseInt($(input#number).val());
-//
-//    if (answer >=3) {
-//     $('#result').show();
-//  }  else {
-//     $('notgoodresult').show();
-//  }
-// });
+  $(".form-group").submit(funtion(event) {
+    alert("Great job!")
+
+    var answer = $(parseInt("1")).val();
+    var answer = $(parseInt("2")).val();
+    var answer = $(parseInt("3")).val();
+
+
+    if (answer >= '3') {
+      $("#greatJob").show();
+      $("#greatJob1").hide();
+    } else {
+       $("thankyou!").show();
+       $("thankyou!1").hide();
+    }
+  });
