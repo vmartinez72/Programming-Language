9b115711299dd81983f6cdca983db1431a98c455
Student <example@example.com>
2018-08-15 19:11:48 -0700

    add if and else statement on js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,34 @@
 $(document).ready(function() {
+ $(".form-group").submit(function(event) {
+   event.preventDefault();
   $(".clickable2").click(function() {
-  // event.preventDefault();
     $("#show").toggle();
     $("#hide").toggle();
+     console.log(".clickabkle2")
   });
   $(".clickable1").click(function() {
     $("#start").toggle();
     $("#end").toggle();
-   });
+     console.log(".clickabkle1")
   });
 
-  $(".form-group").submit(funtion(event) {
-    alert("Great job!")
 
-    var answer = $(parseInt("1")).val();
-    var answer = $(parseInt("2")).val();
-    var answer = $(parseInt("3")).val();
+alert("start");
+ var answer1 = $(parseInt("challenge")).val();
+ var answer2 = $(parseInt("tech")).val();
+ var answer3 = $(parseInt("communication")).val();
+ var answer4 = $(parseInt("visual")).val();
+ var answer5 = $(parseInt("break")).val();
+alert("end");
 
 
-    if (answer >= '3') {
-      $("#greatJob").show();
-      $("#greatJob1").hide();
-    } else {
-       $("thankyou!").show();
-       $("thankyou!1").hide();
-    }
-  });
+  if (answer1 >= 3 || answer2 >=  3  || answer3 3 || answer4 >=  3 || answer5 >= 3) {
+    $("#result").show();
+    $("#notgoodresult").hide();
+   }
+   else {
+    $("thankyou!").show();
+    $("thankyou!1").hide();
+   }
+ });
+});
